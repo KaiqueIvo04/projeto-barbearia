b0a0e8fdce9fb2302444897dec8005c58df4f6e7
Kaique da Silva Ivo <kaiqueivo1011@gmail.com>
2024-05-07 16:40:35 -0300

    Start commit and models finished

---
+++ app.js
@@ -0,0 +1,50 @@
+require('dotenv').config(); //Iniciar configuração do .env
+const Express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./swageer.json');
+
+const athleteRoute = require('./routes/AthleteRoutes');
+const authRoute = require('./routes/AuthRoutes');
+const medicRoute = require('./routes/MedicRoutes');
+const medicalRegisterRoute = require('./routes/MedicalRegisterRoutes');
+const appointmentsRoute = require('./routes/AppointmentRoutes');
+const injuryRoute = require('./routes/InjuryRoutes');
+
+
+const server = new Express(); //Objeto do server
+
+//---------------Middlewares-------------
+server.use(Express.json()); //Para o server entender json
+
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+server.use(helmet());   //Para medidas de segurança
+
+if (process.env.NODE_ENV === 'development') {
+    server.use(require('morgan')('dev')); //Utilizar morgan para feedback do server
+}
+
+//Permitir cors para o front
+server.use(
+    cors({
+        credentials: true,
+        origin: '*',
+    }),
+);
+
+server.use((req, res, next) => {
+    req.requestTime = new Date().toISOString(); //Para pegar a hora em que uma requisição foi feita
+    next();
+})
+
+//Rotas para serem usadas
+server.use('/api/v1/athletes', athleteRoute);
+server.use('/api/v1/auth', authRoute);
+server.use('/api/v1/medics', medicRoute);
+server.use('/api/v1/medicalRegisters', medicalRegisterRoute);
+server.use('/api/v1/appointments', appointmentsRoute);
+server.use('/api/v1/injuryRegisters', injuryRoute);
+
+module.exports = server;
---
+++ models/Client.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = new mongoose.Schema({
+    name: { //Nome
+        type: String,
+        required: [true, "Nome do cliente não definido!"]    //Necessário para criação inicial
+    },
+    password: { //Senha
+        type: String,
+        required: [true, "Senha do cliente não definida!"] //Necessário para criação inicial
+    },
+    email: {    //Email
+        type: String,
+        required: [true, "E-mail do cliente não definido!"] //Necessário para criação inicial
+    },
+    contact_personal: {   //Contato pessoal  
+        type: String,
+        default: ""
+    }
+}, { timestamps: true });   //Adiciona 2 atributos (data que foi criado e data que foi atualizado)
+
+//Função necessária para verificação de senha no login
+Schema.methods.correctPassword = async function (
+    candidatePassword,
+    userPassword
+  ) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+module.exports = mongoose.model("Client", Schema);
